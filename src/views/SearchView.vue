c39a7f16286e41a53f2de6ffcf6cbb6fbd5d35fa
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2023-07-24 14:20:47 +0300

    SearchPage

---
+++ src/views/SearchView.vue
@@ -0,0 +1,59 @@
+<script setup>
+    import CategorySelect from '../components/CategorySelect.vue';
+</script>
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Browse all
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Podcasts" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Audiobooks" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="Made For You" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+            <CategorySelect category="New Release" image="/public/albumCovers/DiariesOfAHero.png"/>
+        </div>
+    </div>
+</template>
