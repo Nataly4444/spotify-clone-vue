f882a70533f7c469d9e745190fe33e5f44f4bb26
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2023-07-24 15:36:17 +0300

    LibraryPage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,11 +14,11 @@
         <div class="py-1.5"></div>
 
         <div class="flex items-center">
-            <HomeCard image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="md:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="lg:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="xl:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="/public/albumCovers/disturbed.jpg" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="/public/albumCovers/soad.webp" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="md:block hidden" image="/public/albumCovers/rammstein.webp" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="lg:block hidden" image="/public/albumCovers/Sabaton-Heroes-2014.jpg" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="xl:block hidden" image="/public/albumCovers/nirvana.jpg" title="Title is here" subTitle="Subtitle is here" />
         </div>
     </div>
 
@@ -33,11 +33,11 @@
         <div class="py-1.5"></div>
 
         <div class="flex items-center">
-            <HomeCard image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="md:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="lg:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="xl:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="/public/albumCovers/rammstein.webp" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="/public/albumCovers/nirvana.jpg" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="md:block hidden" image="/public/albumCovers/gameshow-two-door-cinema.jpg" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="lg:block hidden" image="/public/albumCovers/Queen-Greatest-Hits-Cover-Art.jpeg.webp" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="xl:block hidden" image="/public/albumCovers/soad.webp" title="Title is here" subTitle="Subtitle is here" />
         </div>
     </div>
 
@@ -53,10 +53,10 @@
 
         <div class="flex items-center">
             <HomeCard image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="md:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="lg:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
-            <HomeCard class="xl:block hidden" image="/public/albumCovers/DiariesOfAHero.png" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard image="/public/albumCovers/30sectomars.jpg" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="md:block hidden" image="/public/albumCovers/disturbed.jpg" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="lg:block hidden" image="/public/albumCovers/Sabaton-Heroes-2014.jpg" title="Title is here" subTitle="Subtitle is here" />
+            <HomeCard class="xl:block hidden" image="/public/albumCovers/rammstein.webp" title="Title is here" subTitle="Subtitle is here" />
         </div>
     </div>
 </template>
